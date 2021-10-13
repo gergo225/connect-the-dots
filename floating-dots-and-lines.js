8aa24bb5c91c7f541de572f800689bca842ebb98
gergo225 <fazekasgergo225@gmail.com>
2021-10-13 22:02:43 +0300

    Add p5.js and randomly moving points

--- floating-dots-and-lines.js
+++ floating-dots-and-lines.js
@@ -1,14 +1,73 @@
 const canvasSketch = require('canvas-sketch');
 
+const p5 = require('p5');
+
+new p5();
+
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
+  p5: true,
+  animate: true
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+const sketch = ({ context, width, height }) => {
+  const pointCount = 100;
+  const maxVel = 3;
+  const points = [];
+
+  class Point {
+    constructor(x, y, velX, velY) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+    }
+
+    update() {
+      this.x += this.velX;
+      this.y += this.velY;
+
+      if (this.x < 0 || this.x > width) {
+        this.velX *= -1;
+      }
+      if (this.y < 0 || this.y > height) {
+        this.velY *= -1;
+      }
+    }
+  }
+
+
+
+
+
+  for (let i = 0; i < pointCount; i++) {
+    const x = Math.random() * width;
+    const y = Math.random() * height;
+    const velX = random(-1, 1) * maxVel;
+    const velY = random(-1, 1) * maxVel;
+    points.push(new Point(x, y, velX, velY));
+  }
+
+
+  return () => {
+    fill('white');
+    rect(0, 0, width, height);
+
+    push();
+
+    stroke('black');
+    strokeWeight(20);
+    for (let i = 0; i < points.length; i++) {
+      point(points[i].x, points[i].y);
+      points[i].update();
+    }
+
+    pop();
+
   };
-};
+
+
+
+}
 
 canvasSketch(sketch, settings);
